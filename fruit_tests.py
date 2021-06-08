8be2b9169252e0d0c2dfdaabc332360c8c336ae5
jade963 <jade.stapleton@marsden.school.nz>
2021-06-09 09:56:46 +1200

    This update includes the add_fruit and subtract_fruit functions

--- fruit_tests.py
+++ fruit_tests.py
@@ -1,7 +1,35 @@
-def review_fruit(l):
-    for x in l:
+def get_integer(m):
+    user_input = int(input(m))
+    return user_input
+
+def get_string(m):
+    user_input = input(m)
+    return user_input
+
+def review_fruit(L):
+    for x in L:
         output = "{:<10} -- {:>4}".format(x[0], x[1])
         print(output)
+
+def print_with_indices(L):
+    for i in range((0), len(L)):
+        print("{} : {}".format(i,L[i]))
+
+def add_fruit(L):
+    print_with_indices(L)
+    my_index = get_integer("Please enter the index number of the fruit you would like to add to: ")
+    add_amount = get_integer("Please enter the amount of fruit you would like to add: ")
+    new_amount = L[my_index][1] + add_amount
+    L[my_index][1] = new_amount
+    print("The amount of {} in the bowl has been increased to {}".format(L[my_index][0], new_amount))
+
+def subtract_fruit(L):
+    print_with_indices(L)
+    my_index = get_integer("Please enter the index number of the fruit you would like to subtract from: ")
+    subtract_amount = get_integer("Please enter the amount of fruit you would like to remove: ")
+    new_amount = L[my_index][1] - subtract_amount
+    L[my_index][1] = new_amount
+    print("The amount of {} in the bowl has been decreased to {}".format(L[my_index][0], new_amount))
 
 def main():
 
@@ -11,6 +39,30 @@ def main():
         ["oranges", 6],
         ["grapes", 72],
     ]
+
+    my_menu = '''
+    A: Review bowl contents
+    B: End the program
+    C: Add more of the same kind of fruit
+    D: Remove fruit from the bowl
+    '''
+
+    run = True
+    while run == True:
+        print(my_menu)
+        choice = get_string("Please enter your choice from the menu: ")
+        if choice == "A":
+            review_fruit(fruit_list)
+        elif choice == "B":
+            print("Program has ended")
+            run = False
+        elif choice == "C":
+            add_fruit(fruit_list)
+        elif choice == "D":
+            subtract_fruit(fruit_list)
+        else:
+            print("Unrecognised input, please try again")
+            return main()
     review_fruit(fruit_list)
 
 main()
